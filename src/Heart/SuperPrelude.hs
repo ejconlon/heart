7b38a0c4f3f66cbf5ad7302c8216af2b6b40bf56
Eric Conlon <econlon@lyft.com>
2019-11-02 20:34:04 -0700

    bump

--- src/Heart/SuperPrelude.hs
+++ src/Heart/SuperPrelude.hs
@@ -1,6 +1,8 @@
 module Heart.SuperPrelude
   ( module Heart.Aeson
   , module Heart.App
+  , module Heart.Directory
+  , module Heart.ListT
   , module Heart.Logging
   , module Heart.Prelude
   , module Heart.RIO
@@ -9,6 +11,8 @@ module Heart.SuperPrelude
 
 import Heart.Aeson
 import Heart.App
+import Heart.Directory
+import Heart.ListT
 import Heart.Logging
 import Heart.Prelude
 import Heart.RIO
